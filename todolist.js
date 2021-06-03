1ffcbd9183e9b06fd4c82918c5628fc44b3ff029
lksngy <luk.nagy@gmail.com>
2021-06-03 20:36:46 +0200

    description of the project

---
+++ todolist.js
@@ -0,0 +1,9 @@
+// my own todolist
+
+// db of priorities
+// each day choose 3 that will do your day good and enough to be happy for
+// choose time
+// put into calendar
+// finish - check it EOD
+// keep track of things in github way => similar map of accomplishments
+
